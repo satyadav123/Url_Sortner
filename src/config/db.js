7ffcc8fda59af3226daf99ab5ebf1955e85637a1
satyadav123 <sk259454@gmail.com>
2022-06-02 13:07:53 +0530

    url

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const { default: mongoose } = require("mongoose");
   
  const connectdb=()=>{
 
-    return  mongoose.connect('mongodb+srv://gyan:gyan@cluster0.nm8vq.mongodb.net/dotNetData')
+    return  mongoose.connect("mongodb://127.0.0.1:27017/url_short")
 
  }
  module.exports=connectdb;
